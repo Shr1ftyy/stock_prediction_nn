71bcb077772f1e8825075ae1ff737bf4328f8671
shr1ftyy <syeamtechdemon@gmail.com>
2020-03-27 08:29:13 +0100

    Initial Commit

---
+++ data_api.py
@@ -0,0 +1,133 @@
+# import plotly.graph_objects as go
+import datetime
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from alpha_vantage.techindicators import TechIndicators
+from alpha_vantage.timeseries import TimeSeries
+from matplotlib.pyplot import figure
+
+###############################################
+
+#	This Module is used to parse stock prices
+#	The user can either use AlphaVantage's 
+#	JSON-based API or parse data from a .csv file
+#       
+#       @author Syeam_Bin_Abdullah
+###############################################
+online = True
+
+try:
+	os.system('cls')
+except Exception as e:
+	os.system('clear')
+
+
+print('---- DATA API FOR STOCK PREDICTION ----\n')
+
+def show_option():
+	global option	
+	option = int(input('''Would you like to connect to AlphaVantage
+for Data or select your own csv file?
+
+1. Use AlphaVantage's API (requires an internet connection)
+2. Select a .csv file
+0. Exit
+
+Insert reference number to continue: '''))
+
+show_option()
+print("www")
+
+if option == 1:
+	online = True
+elif option == 2:
+	online = False
+elif option == 0:
+	exit()
+else:
+	print('You did not select the displayed options, please try again')
+	show_option()
+
+# stat.rename(index= pd.to_datetime,
+#             columns = lambda x : x.split(' ')[-1],
+#             inplace = True) 
+
+
+
+if online:
+	try:
+		selection = input("Stock symbol: ")
+		print('Establishing Connection with API...')
+	# Your key here
+		key = '072HERMNM5POYD6B'
+
+		# Choose your output format, or default to JSON (python dict)
+		ts = TimeSeries(key, output_format='pandas')
+		ti = TechIndicators(key)
+		# Get the stat, returns a tuple
+		# stat is a pandas statframe, meta_stat is a dict
+		stat, meta_stat = ts.get_daily(symbol=selection, outputsize= 'full')
+		# sma is a dict, meta_sma also a dict
+		sma, meta_sma = ti.get_sma(symbol=selection)
+		
+		print('--- Connection Established ---')
+		print("\n\nShowing Prices for: {}".format(selection))
+		print(str(stat))
+
+		stat = stat.iloc[::-1]
+		stock_open=stat['open']
+		high=stat['high']
+		low=stat['low']
+		close=stat['close']
+		volume=stat['volume']
+	except Exception as e:
+		stock_open=stat['1. open']
+		high=stat['2. high']
+		low=stat['3. low']
+		close=stat['4. close']
+		volume=stat['5. volume']
+else:
+	selection = input('Insert .csv file path/name: ')
+	selection = selection.split(' ')
+	selection = selection[0]
+	selection = str(selection)
+
+	stat = pd.read_csv(selection)
+
+	stat.index = stat['Date']
+	print("\n\nShowing Prices from csv file: {}".format(selection))
+	print(stat)
+	stock_open=stat['Open']
+	high=stat['High']
+	low=stat['Low']
+	close=stat['Close']
+	volume=stat['Volume']
+
+
+print(f"latest volume: {volume[0]}")	
+
+# Visualization
+
+def visualize():
+
+	figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
+	close.plot()
+	plt.title(f"{selection} Stock Price")
+	plt.ylabel(f'Price')
+	plt.tight_layout()
+	plt.grid()
+	plt.show()
+
+ 
+ 
+# visualize()
+
+# fig = go.Figure(data=[go.Candlestick(x=stat.index,
+#                 open=stat['1. open'],
+#                 high=stat['2. high'],
+#                 low=stat['3. low'],
+#                 close=stat['4. close'])])
+
+# fig.show()
---
+++ nn.py
@@ -0,0 +1,171 @@
+'''
+ ----- To Do: -----
+ -  Figure out how to split data #########################################
+    into Validation and training, testing (if not
+    already done) - EDIT - >>>REVISIT SENTDEX VIDEO<<<
+
+ -  After Validation and loss is all tuned and fixed,
+    move on with inputting more features into the 
+    Neural Network. Figure out how it should be shaped
+    to input them into the network.
+ 
+ -  Look more into Deep Reinforcement Learning 
+ - News Sentiment Analysis (Natural Language Processing, make seperate neural net.
+   or using processing library like BERT) 
+
+ - Normalization vs. Scaling
+
+ - 
+
+'''
+
+import math
+import os
+import random
+import time
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.compat.v1.keras.layers import CuDNNLSTM
+from tensorflow.keras.models import Sequential, load_model
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+
+import data_api as data
+
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.75, allow_growth=True)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+VERSION = 4
+NAME = f'stock_model_v{VERSION}_{data.selection}_{time.time()}.h5'
+
+#state variables    
+window = 20
+row = 1
+length = len(data.stat)-1
+PREDICT_SIZE = 100
+VALIDATION_PCT = 0.15
+
+EPOCHS = 100
+BATCH_SIZE = 32
+
+
+
+stock_open=data.stock_open
+high=data.high
+low=data.low
+data_close_raw=data.close
+data_close = data_close_raw.values
+# np.flip(data.close.values, axis = 0)
+volume=data.volume
+
+close_train = data_close[:len(data_close)-PREDICT_SIZE-int(len(data_close)*VALIDATION_PCT)]
+close_test = data_close[len(data_close)-PREDICT_SIZE:]
+close_validate = data_close[int(len(data_close))-int(len(data_close)*VALIDATION_PCT)-PREDICT_SIZE:len(data_close)-PREDICT_SIZE]
+close_validate = close_validate.reshape(-1,1)
+close_test = close_test.reshape(-1, 1)
+close_train = close_train.reshape(-1,1)
+
+
+#Preprocessing, Scaling
+sc = MinMaxScaler(feature_range=(0, 1))
+training_scaled = sc.fit_transform(close_train)
+print('Scaled training Data')
+
+x_train = []
+y_train = []
+
+
+for i in range(window, len(training_scaled)):
+    x_train.append(training_scaled[i-window:i, 0])
+    y_train.append(training_scaled[i, 0])
+    print(f"""    
+    x_train.append(training_scaled[{i-window}:{i}, 0]) 
+    y_train.append(training_scaled[{i}, 0])""")
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+#reshape     
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))  
+
+
+#### TESTING ####### VALIDATION #### ___ SNIPPED FROM SEGMENT AFTER MODEL AND MODIFIED
+
+print('Scaled validation Data')
+validation_scaled = sc.fit_transform(close_validate)
+
+
+x_validate = []
+y_validate = []
+
+for i in range (window, len(validation_scaled)):
+    x_validate.append(validation_scaled[i-window:i, 0])
+    y_validate.append(validation_scaled[i, 0])
+    print(i)
+
+x_validate, y_validate = np.array(x_validate), np.array(y_validate)
+x_validate = np.reshape(x_validate, (x_validate.shape[0], x_validate.shape[1],1))
+
+#Neural Network 
+model = Sequential()
+
+model.add(CuDNNLSTM(units = 96, return_sequences = True, input_shape = (x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+
+model.add(CuDNNLSTM(units = 96, return_sequences = True))
+model.add(Dropout(0.2))
+
+model.add(CuDNNLSTM(units = 96, return_sequences = True))
+model.add(Dropout(0.2))
+
+model.add(CuDNNLSTM(units=96))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=32))
+model.add(Dense(units=1))
+
+model.compile(optimizer = 'adam', loss= 'mean_squared_error')
+
+# if(not os.path.exists(f'{NAME}')):
+tensorboard = TensorBoard(log_dir="logs\\{}".format(NAME))
+# filepath = "RNN_Final-{epoch:02d}-{val_acc:.3f}"  # unique file name that will include the epoch and the validation acc for that epoch
+# checkpoint = ModelCheckpoint("models\{}.model".format(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')) # saves only the best ones
+model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data = (x_validate, y_validate))
+model.save(f'models\\{NAME}')
+
+
+model = load_model(f'models\\{NAME}')
+
+reverse_data = data_close_raw
+real_stock_price = list(reverse_data[len(reverse_data)-PREDICT_SIZE:])
+
+dataset_total = pd.concat((reverse_data[:len(reverse_data)-PREDICT_SIZE], reverse_data[len(reverse_data)-PREDICT_SIZE:]), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(close_test) - window:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+y_test = []
+x_test = []
+
+for i in range (window, int(PREDICT_SIZE+window)):
+    x_test.append(inputs[i-window:i, 0])
+    y_test.append(inputs[i, 0])
+    # y_test.append(training_scaled[i, 0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1],1))
+
+
+
+predicted_stock_price = model.predict(x_test) 
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+plt.plot(real_stock_price, color = 'red', label = f'Real {data.selection} Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = f'Predicted {data.selection} Stock Price')
+plt.title(f"{data.selection} Price Prediction")
+plt.xlabel('Timesteps')
+plt.ylabel(f'{data.selection} Stock Price')
+plt.legend()
+plt.show()
